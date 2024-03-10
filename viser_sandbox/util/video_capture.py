e5b65ec5f3b42cc04903fd10f40d763d6bbe1911
Jeffrey Hu <hujh14@gmail.com>
2024-03-09 23:16:13 -0800

    retry

--- viser_sandbox/util/video_capture.py
+++ viser_sandbox/util/video_capture.py
@@ -1,13 +1,14 @@
-import queue
 import threading
+import time
+from queue import Queue
 
 import cv2
 
 
 class VideoCapture:
     def __init__(self, name):
-        self.cap = cv2.VideoCapture(name)
-        self.q = queue.Queue()
+        self.name = name
+        self.q = Queue()
         t = threading.Thread(target=self._reader)
         t.daemon = True
         t.start()
@@ -15,15 +16,20 @@ class VideoCapture:
     # read frames as soon as they are available, keeping only most recent one
     def _reader(self):
         while True:
-            ret, frame = self.cap.read()
-            if not ret:
-                break
-            if not self.q.empty():
-                try:
-                    self.q.get_nowait()  # discard previous (unprocessed) frame
-                except Queue.Empty:
-                    pass
-            self.q.put(frame)
+            cap = cv2.VideoCapture(self.name)
+            while True:
+                ret, frame = cap.read()
+                if not ret:
+                    break
+                if not self.q.empty():
+                    try:
+                        self.q.get_nowait()  # discard previous (unprocessed) frame
+                    except Queue.Empty:
+                        pass
+                self.q.put(frame)
+            cap.release()
+            # Wait before trying again
+            time.sleep(10)
 
     def read(self):
         return self.q.get()
